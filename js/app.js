00200d4e8dcc61243476963cdc83e9750d16ea5d
Stelchic <stilyanmtoskov@gmail.com>
2021-09-03 22:35:33 +0100

    user message

--- js/app.js
+++ js/app.js
@@ -7,3 +7,21 @@ alertDiv.innerHTML = `<p><b>Alert:</b> You have unread messages</p>
 const alertClose = document.addEventListener("click", (e) => {
     alertDiv.style.display = "none";
 });
+
+const user = document.querySelector("#userField");
+const message = document.querySelector("#messageField");
+const sendBtn = document.querySelector("#sendMessage");
+
+sendBtn.addEventListener("click", (e) => {
+    if (user.value === "" && message.value === "") {
+        alert("Please fill out the user and the message fields before sending");
+    } else if (user.value === "") {
+        alert("Please fill out the user field before sending");
+    } else if (message.value === "") {
+        alert("Please fill out the message field before sending");
+    } else {
+        alert(`Message successfully sent to: ${user.value}`);
+        user.value = "";
+        message.value = "";
+    }
+});
